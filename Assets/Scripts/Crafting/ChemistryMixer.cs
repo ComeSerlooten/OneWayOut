6f8d9124fc3d6b72ab060ce36bb44de88f1cdec5
ComeSerlooten <71868443+ComeSerlooten@users.noreply.github.com>
2022-12-03 03:11:18 -0500

    Lab Mixer

---
+++ Assets/Scripts/Crafting/ChemistryMixer.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class ChemistryMixer : MonoBehaviour
+{
+
+    public Transform craftedObject;
+    [SerializeField] Transform SpinnyPart;
+    [SerializeField] Transform InsidePos;
+    [SerializeField] Transform EndPos;
+    bool initialCollider = false;
+    bool initialRb = false;
+    [Space]
+    [SerializeField] Transform debugCrafted;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    [ContextMenu("Craft")]
+    public void DebugCrafted()
+    {
+        BringOutCraft(debugCrafted);
+    }
+
+    public void BringOutCraft(Transform crafted)
+    {
+        craftedObject = crafted;
+
+        if (craftedObject.GetComponent<Collider>())
+        {
+            initialCollider = craftedObject.GetComponent<Collider>().enabled;
+            craftedObject.GetComponent<Collider>().enabled = false;
+        }
+
+        if (craftedObject.GetComponent<Rigidbody>())
+        {
+            initialRb = craftedObject.GetComponent<Collider>().enabled;
+            craftedObject.GetComponent<Rigidbody>().isKinematic = true;
+        }
+
+        craftedObject.position = InsidePos.position;
+        craftedObject.gameObject.SetActive(false);
+        Tween spin = SpinnyPart.DOLocalRotate(new Vector3(0, 0, 360 * 5), 3, RotateMode.FastBeyond360).SetEase(Ease.InOutSine)
+        .OnComplete(() =>
+        {
+            craftedObject.gameObject.SetActive(true);
+            craftedObject.DOMove(EndPos.position, 2f).OnComplete(() =>
+            {
+                if (craftedObject.GetComponent<Collider>()) craftedObject.GetComponent<Collider>().enabled = initialCollider;
+                if (craftedObject.GetComponent<Rigidbody>()) craftedObject.GetComponent<Rigidbody>().isKinematic = initialRb;
+            });
+        });
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
