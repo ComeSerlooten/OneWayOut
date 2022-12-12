cb01e3c3ff514d0c7f8d44c8eee9ec74405aeec2
ComeSerlooten <71868443+ComeSerlooten@users.noreply.github.com>
2022-12-11 20:28:43 -0500

    bucket

--- Assets/Scripts/Interactables/PlantGrowInteractable.cs
+++ Assets/Scripts/Interactables/PlantGrowInteractable.cs
@@ -30,6 +30,27 @@ public class PlantGrowInteractable : InteractableObject
         });
     }
 
+    public override bool Use(Item user)
+    {
+        if ((user == target_ || user.itemName == target_.itemName) && activatable && !(triggerOnce && triggered))
+        {
+            if(user.GetComponent<BucketContentToggle>())
+                if(user.GetComponent<BucketContentToggle>().filled)
+                {
+                    TriggerSequence.Invoke();
+                    ActivationSequence();
+                    user.GetComponent<BucketContentToggle>().Empty();
+                    triggered = true;
+                    return true;
+                }
+
+            return false;
+        }
+        else
+        {
+            return false;
+        }
+    }
 
     // Update is called once per frame
     void Update()
